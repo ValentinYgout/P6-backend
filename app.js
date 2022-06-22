ce1e926021603e718e916ff21db7a7a175fdc4a3
ValentinYgout <ValentinYgout@gmail.com>
2022-06-22 17:18:53 +0200

    test git ignore
    git rm -r --cached <file>

--- app.js
+++ app.js
@@ -35,18 +35,18 @@ app.use((req, res, next) => {
     next();
 });
 app.use(bodyParser.json());
-// app.use(helmet());
-// app.use(mongoSanitize());
+app.use(helmet());
+app.use(mongoSanitize());
 
-// const limiter = rateLimit({
-// 	windowMs: 15 * 60 * 1000, // 15 minutes
-// 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-// 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-// })
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
-// // Apply the rate limiting middleware to all requests
-// app.use(limiter)
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 
 
